3bb3587dce29863c3ae75417013c70ed500b6585
adubnik <adubnik@gmail.com>
2017-02-23 16:03:52 +0000

    inventory fix

--- ansible/scripts/init.sh
+++ ansible/scripts/init.sh
@@ -20,7 +20,7 @@ INVENTORY_DIR="${CUR_DIR}/../inventory"
 PLAYBOOKS_DIR="${CUR_DIR}/../playbooks"
 
 check_inventory() {
-	inv_dir=$(dirname $1)
+	inv_dir=$1
 	if [ ! -e "${inv_dir}/group_vars" ]; then
 		echo "Inventory directory ${inv_dir} is missing group_vars directory"
 		exit 1
